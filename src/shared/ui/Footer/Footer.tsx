7bce5e80a3607bc353dd898a531d0a5c2382b1d4
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-08-29 15:48:55 +0600

    fix footer bag.
    remove Wrapper component inside footer

--- src/shared/ui/Footer/Footer.tsx
+++ src/shared/ui/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 
-import { Wrapper } from "../Wrapper/Wrapper";
 import { PTag } from "../Paragraph/PTag";
 
 import { LinkType } from "../../types/types";
@@ -35,47 +34,45 @@ export const Footer = () => {
 
   return (
     <div className={styles.root}>
-      <Wrapper>
-        <div className={styles.footer}>
-          <div>
-            <PTag variant="md" className={styles.title}>
-              Сайта по самовыявлению симптомов и признаков колоректального рака
-            </PTag>
-            <PTag className={styles.content}>
-              Опросник для самостоятельной диагностики признаков и симптомов
-              колоректального рака
-            </PTag>
-          </div>
-          <div>
-            <PTag variant="md" className={styles.title}>
-              Полезная информация
-            </PTag>
-            <ul className={styles.content}>
-              {inform.map((link) => (
-                <li key={link.path}>
-                  <NavLink style={{ color: "#fff" }} to={link.path}>
-                    {link.title}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <PTag variant="md" className={styles.title}>
-              Поддержка
-            </PTag>
-            <ul className={styles.content}>
-              {support.map((link) => (
-                <li key={link.path}>
-                  <NavLink style={{ color: "#fff" }} to={link.path}>
-                    {link.title}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
+      <div className={styles.footer}>
+        <div>
+          <PTag variant="md" className={styles.title}>
+            Сайта по самовыявлению симптомов и признаков колоректального рака
+          </PTag>
+          <PTag className={styles.content}>
+            Опросник для самостоятельной диагностики признаков и симптомов
+            колоректального рака
+          </PTag>
         </div>
-      </Wrapper>
+        <div>
+          <PTag variant="md" className={styles.title}>
+            Полезная информация
+          </PTag>
+          <ul className={styles.content}>
+            {inform.map((link) => (
+              <li key={link.path}>
+                <NavLink style={{ color: "#fff" }} to={link.path}>
+                  {link.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <PTag variant="md" className={styles.title}>
+            Поддержка
+          </PTag>
+          <ul className={styles.content}>
+            {support.map((link) => (
+              <li key={link.path}>
+                <NavLink style={{ color: "#fff" }} to={link.path}>
+                  {link.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
