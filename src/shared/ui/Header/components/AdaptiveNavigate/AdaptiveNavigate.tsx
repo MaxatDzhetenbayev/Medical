38609326eca561e511cca90ab81549a9d3c090e0
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-09-04 13:48:14 +0600

    add animation adaptive menu

--- src/shared/ui/Header/components/AdaptiveNavigate/AdaptiveNavigate.tsx
+++ src/shared/ui/Header/components/AdaptiveNavigate/AdaptiveNavigate.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-
+import { motion, Variants } from "framer-motion";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 import styles from "./AdaptiveNavigate.module.scss";
@@ -14,6 +14,30 @@ type Props = {
 export const AdaptiveNavigate = ({ linkList, onClose }: Props) => {
   const navigate = useNavigate();
 
+  const parentvariants: Variants = {
+    open: {
+      visibility: "visible",
+      padding: "10px 20px",
+      height: 400,
+      transition: {
+        type: "spring",
+        delayChildren: 0.1,
+        staggerChildren: 0.05,
+      },
+    },
+    closed: {
+      height: 0,
+      padding: "0px 20px",
+      visibility: "hidden",
+      transition: {
+        type: "spring",
+        delay: 1,
+        delayChildren: 0.1,
+        staggerChildren: 0.05,
+      },
+    },
+  };
+
   const [showSubMenu, setShowSubMenu] = useState(false);
 
   const goToPath = (children: any, path: string) => {
@@ -21,7 +45,7 @@ export const AdaptiveNavigate = ({ linkList, onClose }: Props) => {
       navigate(path);
       onClose();
     }
-    setShowSubMenu(true);
+    setShowSubMenu(!showSubMenu);
   };
 
   return (
@@ -40,16 +64,44 @@ export const AdaptiveNavigate = ({ linkList, onClose }: Props) => {
                   {icon}
                   {title()}
                 </button>
-                {children && showSubMenu && (
-                  <ul className={styles.nested_links}>
-                    {children.map((link) => (
-                      <li key={link.path} className={styles.nested_link}>
+                {children && (
+                  <motion.ul
+                    initial={false}
+                    animate={showSubMenu ? "open" : "closed"}
+                    variants={parentvariants}
+                    className={styles.nested_links}
+                  >
+                    {children.map((link, index) => (
+                      <motion.li
+                        variants={{
+                          open: {
+                            opacity: 1,
+                            transition: {
+                              type: "spring",
+                              stiffness: 300,
+                              damping: 15,
+                              delay: index * 0.1,
+                            },
+                          },
+                          closed: {
+                            opacity: 0,
+                            transition: {
+                              type: "spring",
+                              stiffness: 300,
+                              damping: 15,
+                              delay: index * 0.1,
+                            },
+                          },
+                        }}
+                        key={link.path}
+                        className={styles.nested_link}
+                      >
                         <NavLink to={link.path} onClick={onClose}>
                           {link.title}
                         </NavLink>
-                      </li>
+                      </motion.li>
                     ))}
-                  </ul>
+                  </motion.ul>
                 )}
               </li>
             ))}
